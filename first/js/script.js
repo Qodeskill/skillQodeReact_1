b836c044053d69fb8c045a4e7cf3a8fa7c968586
paresh skill qode <skillqode.paresh@gmail.com>
2022-02-26 12:42:11 +0530

    2602

---
+++ first/js/script.js
@@ -0,0 +1,56 @@
+// 26/02/2022
+// document.write("Hello")
+// console.log("Hello")
+
+// var a = 10
+// var b = 20
+// console.log("a : ", a)
+// console.log("b : ", b)
+// console.log("a+b : ", a)//concate
+// console.log("a+b : ", a + b)
+// console.log("a+b : " + (a + b))
+
+// var str = "skill"
+// var digit = 10
+// var decimal = 3.14
+// var char = 'a'
+
+// var digitChar = digit + char
+// var digitDecimal = digit + decimal
+// console.log(digitChar)
+// console.log(digitDecimal)
+// var digitChar = digit + char
+// var digitChar = digit + char
+// typeof
+
+// console.log(typeof (str))
+// console.log(typeof (digit))
+// console.log(typeof (decimal))
+// console.log(typeof (char))
+// console.log()
+
+
+// digit = digit + char
+// var digitDecimal = digit + decimal
+// console.log(typeof (digit))
+// console.log(digitDecimal)
+
+var a = 95
+var b = '97'
+var b1 = "9"
+
+console.log(b == 'a')
+// console.log(a > b)
+// console.log(typeof (a))
+// console.log(typeof (b))
+// console.log(typeof (b1))
+
+// console.log(a == b)//value
+// console.log(a === b)//check datatype
+// console.log(a > b)
+// if (a > b)
+//     console.log("a is greater...")
+// else
+//     console.log("b is greater...")
+
+
