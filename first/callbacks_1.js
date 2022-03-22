1418bb02d149f7264012de05fa1ecbbd6abefc14
paresh skill qode <skillqode.paresh@gmail.com>
2022-03-22 11:48:41 +0530

    22/03/22 callbackhell

--- first/callbacks_1.js
+++ first/callbacks_1.js
@@ -30,19 +30,66 @@
 
 
 
-function fun_one(param1) {
-    // console.log(param1)
-    // console.log(param1("Hello_1"));
+// function fun_one(param1) {
+// console.log(param1)
+// console.log(param1("Hello_1"));
 
-    return param1("Hello_1");
-    // return "Hello_1";
-}
+// return param1("Hello_1");
+// return "Hello_1";
+// }
 // let my_func = (arg1, arg2, arg3) => { console.log(arg1, arg2, arg3); }
-let my_func = (arg1) => {
-    // console.log(arg1);
-    // console.log(arg1);
-    return arg1;
-}
-console.log(fun_one(my_func));
-    //Hello_1 Hello_2 Hello_3
+// let my_func = (arg1) => {
+// console.log(arg1);
+// console.log(arg1);
+// return arg1;
+// }
+// console.log(fun_one(my_func));
+//Hello_1 Hello_2 Hello_3
+
+
+// function fun_one(param1) {
+//     return param1("Hello_1", "Hello_2", "Hello_3");
+// }
+// fun_one((arg1, arg2, arg3) => {
+//     console.log(arg1, arg2, arg3);
+// });
+//Hello_1 Hello_2 Hello_3
+
+
+// function fun_one(param1, param2, param3) {
+//     return param1("Hello_1") + " - " + param2("Hello_2") + " - " + param3("Hello_3");
+// }
+// fun_one((arg1) => { console.log(arg1) }, (arg2) => { console.log(arg2) }, (arg3) => { console.log(arg3) })
+/*
+    function fun_one(param1,param2,param3){
+        return param1("Hello_1")+"...."+param2("Hello_2")+"...."+param3("Hello_3");
+    }
+    fun_one( (arg1)=>{
+        console.log( arg1 );                //Hello_1
+    }, (arg1)=>{
+        console.log( arg1 );                //Hello_2
+    }, (arg1)=>{
+        console.log( arg1 );                //Hello_3
+    } );
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
