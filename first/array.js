882eca29fbef0b69c3c26c2b6d18002b6a44be7a
paresh skill qode <skillqode.paresh@gmail.com>
2022-04-06 12:05:38 +0530

    06042022

--- first/array.js
+++ first/array.js
@@ -102,38 +102,44 @@
 // The slice() method returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) where start and end represent the index of items in that array. The original array will not be modified.
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
 // [5,7)
-let arr1 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+// let arr1 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 //  E               S
 // newarr = arr1.slice(5, 7)
 // console.log(newarr);
 // console.log(arr1.slice(5, 7));
 // console.log(arr1.slice(1, -4));
 // console.log(arr1.slice(-5, -1))
-console.log(arr1.slice(7, 1));
-/*
-    //findIndex()
-    let arr = [10,20,30,40,50,60,70,80,90,100];
-    console.log(
-        arr.findIndex((element,index)=>{
-            return element == 50;
-        })
-    );             console.log(
-        arr.findIndex((element,index)=>{
-            return element == 80;
-        })
-    );         console.log(
-        arr.findIndex((element,index)=>{
-            return element == 1000;
-        })
-    );             arr.splice(arr.findIndex((element,index)=>{
-        return element == 50;
-    }),1);
-    console.log( arr );
-    arr.splice( arr.findIndex((element,index)=>{
-        return element == 90;
-    }), 1);
-    console.log( arr );
-*/
+// console.log(arr1.slice(7, 1));
+
+// findIndex()
+// The findIndex() method returns the index of the first element in the array that satisfies the provided testing function. Otherwise, it returns -1, indicating that no element passed the test.
+
+
+// let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+// let n = arr.findIndex((element, index) => {
+//     return element > 101;
+// })
+// console.log(n); //4
+// console.log(
+//     arr.findIndex((element, index) => {
+//         return element == 80;
+//     })
+// );
+// console.log(
+//     arr.findIndex((element, index) => {
+//         return element == 1000;
+//     })
+// );
+// arr.splice(arr.findIndex((element, index) => {
+//     return element == 50;
+// }), 1);
+// console.log(arr);
+// arr.splice(arr.findIndex((element, index) => {
+//     return element == 90;
+// }), 1);
+// console.log(arr);
+
 
 
 /*
@@ -194,62 +200,66 @@ console.log(
 
 */
 
+
+//reverse()
+//reverse() function used to reverse the array elements
+// let arr1 = [10, 20, 30, 40, 50];
+// console.log(arr1.reverse());
+//[ 50, 40, 30, 20, 10 ]
+// let arr2 = ["Angular14", "ReactJS", "NodeJS", "VueJS", "MongoDB"];
+// console.log(arr2.reverse());
+//[ 'MongoDB', 'VueJS', 'NodeJS', 'ReactJS', 'Angular14' ]
+// console.log(
+//     // "Hello".split("")
+//     "Hello".split("").reverse().join("")
+// );
+
+// arr = ['H', 'e', 'l', 'l', 'o']
+// console.log(arr.join(""))
+
+
+
+
 /*
-    //reverse()
-    //reverse() function used to reverse the array elements
-    let arr1 = [10,20,30,40,50];
-    console.log( arr1.reverse() );
-    //[ 50, 40, 30, 20, 10 ]
-    let arr2 = ["Angular14","ReactJS","NodeJS","VueJS","MongoDB"];
-    console.log( arr2.reverse() );
-    //[ 'MongoDB', 'VueJS', 'NodeJS', 'ReactJS', 'Angular14' ]
-    console.log(
-        "Hello".split("").reverse().join("")
-    );
-
-*/
-
-
-
-/*
-let arr1 = ["Angular14","ReactJS","NodeJS","VueJS","MongoDB"];
+let arr1 = ["Angular14", "ReactJS", "NodeJS", "VueJS", "MongoDB"];
 
 console.log(
-    arr1.map((element,index)=>{
+    arr1.map((element, index) => {
         return element.split("").reverse().join("")
     })
 );
 console.log(
-    arr1.map((element,index)=>{
+    arr1.map((element, index) => {
         return element === "NodeJS" ? element.split("").reverse().join("") : element
     })
-);
-*/
-
-
-/*
-    //...
-    //spread operator
-    let arr1 = ["Angular14"];
-    let arr2 = ["NodeJS"];
-    let arr3 = ["MongoDB"];
-    let arr4 = [...arr1,...arr2,...arr3];
-    console.log(arr4);      //[ 'Angular14', 'NodeJS', 'MongoDB' ]
-    let father = ["1cr"];
-    let child = [...father,"1kg gold"];
-    let grand_child = [...child,"1kg silver"];
-    console.log( grand_child );     //[ '1cr', '1kg gold', '1kg silver' ]
-*/
+);*/
 
 
 
+//...
+//spread operator ...
+// let arr1 = ["Angular14"];
+// let arr2 = ["NodeJS", "React"];
+// let arr3 = ["MongoDB", "SQL", "cassandra"];
+// let arr4 = [...arr1, "Vue", ...arr2];
 
-// let arr = [10, 20, 30, 10, 20, 30];
-//indexOf()
-//it won't create indexes to repeated elements
+// console.log(...arr2);
+// console.log(arr4);
+// let father = ["1cr"];
+// let child = [...father, "1kg gold"];
+// let grand_child = [...child, "1kg silver"];
+// console.log(grand_child);     //[ '1cr', '1kg gold', '1kg silver' ]
 
-// arr.forEach((element,index)=>{
-//     console.log( arr.indexOf(element) );
+
+
+
+
+// let arr = [10, 20, 30, 40, 10, 20, 30];
+// indexOf()
+// it won't create indexes to repeated elements
+
+// arr.forEach((element, index) => {
+//     console.log(arr.indexOf(element));
 // })
 
 // console.log(
