82e644bbcb50268da6ab2d419c84b2fabae21033
paresh skill qode <skillqode.paresh@gmail.com>
2022-05-23 10:56:52 +0530

    commit list rendering

--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom/client'; import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
+import "./index.css";
+import "font-awesome/css/font-awesome.css";
 import reportWebVitals from './reportWebVitals';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
